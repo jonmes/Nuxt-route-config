0639b0c3e9bf502fa3911028a20d86c9c78f40c3
joni <mesfinjohn@rocketmail.com>
2022-07-07 15:29:27 +0300

    tailwind installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    // router: {
-    //     scrollBehavior: () => {
-    //         console.log('scroll behavioru ');
-    //         return  {x: 0, y: 0} 
-    //         },
-    //   },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });

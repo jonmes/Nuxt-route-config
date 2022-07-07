64a27fa1fee829716ea79b05c9021e89010de7a1
joni <mesfinjohn@rocketmail.com>
2022-07-07 15:44:23 +0300

    nuxt/tailwind && nuxt/color-mode added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
-
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   postcss: {
     plugins: {
       tailwindcss: {},
